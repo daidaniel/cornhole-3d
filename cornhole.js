fbd7e0ce558c2f218e6130d691b5ae8ed6ddb60d
achen180 <103001244+achen180@users.noreply.github.com>
2022-11-10 01:32:04 -0800

    Collision Detection and Reshoot

--- cornhole.js
+++ cornhole.js
@@ -24,6 +24,15 @@ export class Cornhole extends Scene {
             plastic: new Material(new defs.Phong_Shader(),
                 { ambient: .4, diffusivity: .6, color: hex_color("#ffffff") }),
         };
+
+        this.starttime = 0.0;
+        this.currenttime = 0.0;
+    }
+
+    make_control_panel() {
+        this.key_triggered_button("Shoot", ["c"], () => {
+            this.starttime = this.currenttime;
+        });
     }
 
     display(context, program_state) {
@@ -34,8 +43,9 @@ export class Cornhole extends Scene {
         if (!context.scratchpad.controls) {
             this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
             // Define the global camera and projection matrices, which are stored in program_state.
-            program_state.set_camera(Mat4.translation(0, -10, -30));
+            program_state.set_camera(Mat4.translation(0, -2, 0));
         }
+        const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
         program_state.projection_transform = Mat4.perspective(
             Math.PI / 4, context.width / context.height, 1, 100);
 
@@ -48,16 +58,32 @@ export class Cornhole extends Scene {
         let floor_color = color(.4, .8, .4, 1);
         this.shapes.cube.draw(context, program_state, floor_transform, this.materials.plastic.override({ color: floor_color }));
 
+        // Cornhole
+        let cornhole_color = color(0.6, 0.3, 0, 1);
+        let cornhole_x = 10 * Math.sin(t) + 15.5;
+        let cornhole_z = 10 * Math.sin(-1 * t) - 15.5
+        let cornhole_loc =  Mat4.identity().times(Mat4.translation(cornhole_x, 0.5, cornhole_z));
+        this.shapes.cube.draw(context, program_state, cornhole_loc, this.materials.plastic.override({color : cornhole_color}));
+
         // Bean Bag
-        const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
         let init_pos = vec3(0, 10, 0);
         let vel = vec3(10, 20, -10);
         let acc = vec3(0, -32.17, 0); // ft/s^2
-        let pos = init_pos.plus(vel.times(t)).plus(acc.times(.5 * t * t));
+        let newt = t - this.starttime;
+        this.currenttime = t;
+        let pos = init_pos.plus(vel.times(newt)).plus(acc.times(.5 * newt * newt));
 
         let beanbag_transform = Mat4.identity().times(Mat4.translation(pos[0], pos[1], pos[2]));
         let beanbag_color = color(.8, .4, .4, 1);
         this.shapes.sphere.draw(context, program_state, beanbag_transform, this.materials.plastic.override({ color: beanbag_color }));
+
+        let xcollision = (Math.floor(pos[0]) <= cornhole_x + 1 && Math.floor(pos[0]) >= cornhole_x - 1);
+        let ycollision = (pos[1] <= 0.5 && pos[1] >= 0);
+        let zcollision = (Math.floor(pos[2]) <= cornhole_z + 1 && Math.floor(pos[2]) >= cornhole_z - 1);
+
+        if (xcollision && ycollision && zcollision) {
+            console.log(1);
+        }
 
         // Bean Bag Trajectory
         let traj_show = true; // "throw" control should turn this off
